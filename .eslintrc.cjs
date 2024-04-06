c0d57799b28e3d69baf6c8ebdafd7c0edfb12846
Benjamin FABRY <fabrybenjamin5@gmail.com>
2024-04-06 19:18:36 +0200

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,63 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'import'],
+  extends: [
+    'preact',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings'
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: false,
+  },
+  ignorePatterns: ['.eslintrc.cjs'],
+  rules: {
+    'jest/no-deprecated-functions': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'sort-imports': [
+      'error',
+      {
+        'ignoreCase': true,
+        'ignoreDeclarationSort': true
+      }
+    ],
+    'import/order':
+      [
+        'error',
+        {
+          'groups': ['builtin', 'external', 'parent', 'sibling', 'index']
+        }
+      ]
+  },
+  'settings': {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.ts', '.tsx']
+      },
+      'typescript': {
+        'alwaysTryTypes': true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // Choose from one of the 'project' configs below or omit to use <root>/tsconfig.json by default
+
+        // use <root>/path/to/folder/tsconfig.json
+        'project': '<root>/tsconfig.json',
+      }
+    },
+    'import/internal-regex': '^@/'
+  },
+};
