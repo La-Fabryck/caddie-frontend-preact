aec550ac89e0b2ff36ba97f208e1e218919255e9
Olivia Hick <oliviahick1@gmail.com>
2024-12-16 17:45:40 +0100

    feat: shopping list page style

--- src/pages/ShoppingList/index.tsx
+++ src/pages/ShoppingList/index.tsx
@@ -9,18 +9,46 @@ function printLists(items: Item[]): JSX.Element {
   }
 
   return (
-    <table>
-      <tr>
-        <th>id</th>
-        <th>name</th>
-      </tr>
-      {items.map((item) => (
-        <tr key={item.id}>
-          <td>{item.id}</td>
-          <td>{item.name}</td>
-        </tr>
-      ))}
-    </table>
+    <ul>
+      <div className="relative flex flex-col rounded-xl bg-crust">
+        <nav className="flex min-w-[240px] flex-col gap-1 p-2">
+          {items.map((item) => (
+            <div
+              key={item.id}
+              role="button"
+              className="flex w-full items-center rounded-lg p-0 transition-all hover:bg-base focus:bg-slate-100 active:bg-slate-100"
+            >
+              <label htmlFor={item.id} className="flex w-full cursor-pointer items-center px-3 py-2 ">
+                <div className="inline-flex items-center">
+                  <label className="flex items-center cursor-pointer relative" htmlFor={item.id}>
+                    <input
+                      type="checkbox"
+                      className="peer h-5 w-5 cursor-pointer transition-all appearance-none rounded shadow hover:shadow-md border border-pink checked:bg-pink"
+                      id={item.id}
+                    />
+                    <svg
+                      className="absolute w-3 h-4 pointer-events-none hidden peer-checked:block stroke-crust ml-1 outline-none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <polyline points="20 6 9 17 4 12"></polyline>
+                    </svg>
+                  </label>
+                  <label className="cursor-pointer ml-2 text-sm" htmlFor={item.id}>
+                    {item.name}
+                  </label>
+                </div>
+              </label>
+            </div>
+          ))}
+        </nav>
+      </div>
+    </ul>
   );
 }
 
@@ -44,9 +72,13 @@ export const ShoppingList: FC = () => {
   }
 
   return (
-    <div className="home">
-      {list != null && <h1>{list.title}</h1>}
-      {items != null && printLists(items)}
+    <div className="py-5">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0 my-5">
+          <h2 className="text-4xl font-semibold tracking-tight sm:text-5xl">{list?.title || 'Ma liste'}</h2>
+        </div>
+        {items != null && printLists(items)}
+      </div>
     </div>
   );
 };
